b0a221732627710f7975c8f63cb2d9fab6bf0ea3
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2026-09-12 22:18:47 -0600

    feat: write custom prompt

--- home/fish/default.nix
+++ home/fish/default.nix
@@ -3,9 +3,24 @@
     enable = true;
     interactiveShellInit = ''
       set fish_greeting
-      fish_config prompt choose scales
 
       abbr -a e $EDITOR
+      abbr -a cc "g++ -std=c++23 solve.cpp && ./a.out<1"
     '';
+    functions = {
+      fish_prompt = ''
+        set -l last_status $status
+
+        set -l stat
+        if test $last_status -ne 0
+            set stat (set_color red)" ($last_status)"
+        end
+
+        printf "%s%s%s %sλ %s" (set_color blue) (prompt_pwd) "$stat" (set_color bryellow) (set_color --reset)
+      '';
+      fish_right_prompt = ''
+        printf "%s%s%s" (set_color blue) (fish_vcs_prompt) (set_color --reset)
+      '';
+    };
   };
 }
